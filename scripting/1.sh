a96c3adcba18ac165f8eae663fa3d26e044b66bc
Gabrielcamisadez <gabriel448521@gmail.com>
2025-12-15 16:20:38 -0300

    .

--- scripting/1.sh
+++ scripting/1.sh
@@ -3,20 +3,23 @@
 test='intro to shell scripting'
 echo $test
 
-#yy=("172.16.60.20" "172.16.60.21" "172.16.60.23")
+yy=("172.16.60.20" "172.16.60.21" "172.16.60.23")
 
 echo "${yy[@]}"
 
 number=1
 ip=172.16.60.
 
-while [ $number -lt 20 ];do
-	ping -c 1 $ip$number 
-	echo "\n"
-	number=$((number + 1))
+#while [ $number -lt 20 ];do
+#	ping -c 1 $ip$number 
+#	echo "\n"
+#	number=$((number + 1))
+#done
+
+for ip in $yy
+do
+	host $ip
 done
-
-
 
 
 
