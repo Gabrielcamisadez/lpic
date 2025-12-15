8898bc84c287cdbf0edc9615ef60320c2dbf528e
Gabrielcamisadez <gabriel448521@gmail.com>
2025-12-15 15:59:24 -0300

    .

--- scripting/1.sh
+++ scripting/1.sh
@@ -7,5 +7,10 @@ yy=("test" "2" "blue")
 
 echo "${yy[@]}"
 
+number=10
 
-
+if [ $number -gt 0 ]; then
+  echo "The number is positive."
+elif [ $number -lt 0 ]; then
+  echo "The number is negative."
+fi
